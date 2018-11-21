16405786930467d923c0a82a4265a92147410d67
David Carl <david.martin.carl@gmail.com>
2018-11-21 11:08:54 +0100

    test

---
+++ python_lib/flask/static/styles/table.js
@@ -0,0 +1,45 @@
+table {
+    margin: auto;
+    border-collapse: collapse;
+}
+
+th, td {
+    border: 1px solid #ddd;
+    width: 200px;
+}
+
+tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+tr:hover {
+    background-color: #ddd;
+}
+
+th {
+    background-color: #3a88d1;
+}
+
+td {
+    padding: 5px;
+}
+
+.double-size-table {
+    width: 497px;
+}
+
+.worked {
+    width: 150px;
+}
+
+.totalwork {
+    padding-top: 13px;
+}
+
+.timetable {
+    padding-bottom: 12px;
+}
+
+.issue {
+    width: 75px;
+}
